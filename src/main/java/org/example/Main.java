082f3b21cf997a995bcdad47dca858d0431ab290
ognyp <Oksankagnyp@gmail.com>
2023-12-10 21:57:46 +0200

    Added comments to Calculator class and addCorrectEquationToDB method

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,11 +1,37 @@
 package org.example;
 
+import java.sql.*;
+
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
 
+        String equation = "2*x+5=17";
+        addCorrectEquationToDB(equation);
+    }
 
-        System.out.println(Calculator.isEquationCorrect(" -*(x+5+x)+5=10"));
+    private static void addCorrectEquationToDB(String equation){
+
+        String jdbcUrl = "jdbc:postgresql://localhost:5432/Equation";
+        String username = "postgres";
+        String password = "4321";
+
+        if (Calculator.isEquationCorrect(equation)) {
+
+            try {
+                Connection connection = DriverManager.getConnection(jdbcUrl, username, password);
+                System.out.println("Connected");
+                String sql = "INSERT INTO test (equation) VALUES (?)";
+
+                PreparedStatement statement = connection.prepareStatement(sql);
+                statement.setString(1, equation);
+
+                statement.executeUpdate();
+                connection.close();
+            } catch (SQLException ex) {
+                ex.printStackTrace();
+            }
+        }
     }
 }
